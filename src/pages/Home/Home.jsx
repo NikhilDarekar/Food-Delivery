22032d37b6f9dc8017307711f1e0aeb8b0408b3c
NikhilDarekar <nikhilx97@gmail.com>
2026-03-09 20:04:31 +0530

    Added second page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Layout from '../../components/Layouts/Layout.jsx'
 import Section1 from './Section1.jsx'
 import "../../styles/HomeStyle.css"
+import Section2 from './Section2.jsx'
+import Section3 from './Section3.jsx'
 
 function Home() {
   return (
     <>
       <Layout>
          <Section1/>
+         <Section2  />
+         <Section3/>
       </Layout> 
     </>
   )
