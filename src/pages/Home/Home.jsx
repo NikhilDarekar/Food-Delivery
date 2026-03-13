36be07435856a8dcb20ec5491fc3a040e5bb2723
NikhilDarekar <nikhilx97@gmail.com>
2026-03-13 11:31:24 +0530

    adding carousel effect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,15 +5,17 @@ import "../../styles/HomeStyle.css"
 import Section2 from './Section2.jsx'
 import Section3 from './Section3.jsx'
 import Section4 from '../Section4.jsx'
+import Section5 from './Section5.jsx'
 
 function Home() {
   return (
     <>
       <Layout>
-         <Section1/>
-         <Section2  />
-         <Section3/>
-         <Section4/>
+         <Section1 />
+         <Section2 />
+         <Section3 />
+         <Section4 />
+         <Section5 />
       </Layout> 
     </>
   )
---
+++ src/pages/Home/Section5.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
+import StoreIOS from '../../assets/shop/appstore.png'
+import StoreGoogle from '../../assets/shop/appstore.png'
+import { Link } from 'react-router-dom';
+import DownloadImage from '../../assets/shop/e-shop.png'
+import Brand1 from '../../assets/brands/brand-11.png'
+import Brand2 from '../../assets/brands/brand-12.png'
+import Brand3 from '../../assets/brands/brand-13.png'
+import Brand4 from '../../assets/brands/brand-14.png'
+import Brand5 from '../../assets/brands/brand-15.png'
+import Brand6 from '../../assets/brands/brand-16.png'
+import Brand7 from '../../assets/brands/brand-17.png'
+import Brand8 from '../../assets/brands/brand-18.png'
+
+
+
+
+function Section5() {
+  return (
+    <>
+      <section className='shop_section'>
+        <Container>
+          <Row className='align-items-center'>
+            <Col lg={6} className='text-center text-lg-start mb-5 mb-lg-0'>
+              <h4>Download Mobile App and</h4>
+              <h2>Save upto 20%</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, ipsum?</p>
+              <Link to='/'>  
+                  <img src={StoreIOS} alt='IOS' className='store img-fluid me-3 '></img>
+              </Link>
+              <Link to='/'>  
+                  <img src={StoreGoogle} alt='Android' className='store img-fluid me-3 '></img>
+              </Link>
+            </Col>
+            <Col lg={6}>
+              <Link to='/'>
+                <img src={DownloadImage} alt='Download' className='img-fluid'></img>
+              </Link>
+                  
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className='brand_section'>
+        <Container>
+          <Row>
+            <Carousel>
+                <Carousel.Item> 
+                  <Carousel.Caption>
+                    <div className='d-flex align-items-center justify-content-between'>
+                      <div className='brand_img'>
+                          <img src={Brand1} alt='brand-1' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand2} alt='brand-2' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand3} alt='brand-3' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand4} alt='brand-4' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand5} alt='brand-5' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand6} alt='brand-6' className='img-fluid' />
+                      </div>
+                    </div>
+                  </Carousel.Caption>
+                </Carousel.Item>  
+                <Carousel.Item> 
+                  <Carousel.Caption>
+                    <div className='d-flex align-items-center justify-content-between'>
+                      <div className='brand_img'>
+                          <img src={Brand3} alt='brand-3' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand4} alt='brand-4' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand5} alt='brand-5' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand6} alt='brand-6' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand7} alt='brand-7' className='img-fluid' />
+                      </div>
+                      <div className='brand_img'>
+                          <img src={Brand8} alt='brand-8' className='img-fluid' />
+                      </div>
+                    </div>
+                  </Carousel.Caption>
+                </Carousel.Item>  
+              </Carousel>
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
+
+export default Section5
