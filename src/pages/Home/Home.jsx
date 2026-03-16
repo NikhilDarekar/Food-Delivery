986cfc56ea6b3ffc81a6b5989e9f062867ad4f1b
NikhilDarekar <nikhilx97@gmail.com>
2026-03-16 17:38:58 +0530

    added contact us page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,8 @@ import Section2 from './Section2.jsx'
 import Section3 from './Section3.jsx'
 import Section4 from '../Section4.jsx'
 import Section5 from './Section5.jsx'
+import Section6 from './Section6.jsx'
+import Section7 from './Section7.jsx'
 
 function Home() {
   return (
@@ -16,6 +18,8 @@ function Home() {
          <Section3 />
          <Section4 />
          <Section5 />
+         <Section6 />
+         <Section7 />
       </Layout> 
     </>
   )
